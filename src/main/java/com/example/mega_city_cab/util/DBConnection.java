9ab809963837990afc2bb9e5475f371b63a134ad
Kulan66 <kulanthamuditha66@gmail.com>
2025-02-15 21:19:34 +0530

    database connection

---
+++ src/main/java/com/example/mega_city_cab/util/DBConnection.java
@@ -0,0 +1,30 @@
+package com.example.mega_city_cab.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/Mega_City_Cab";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Kulan@2003";
+    private static Connection connection = null;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            } catch (ClassNotFoundException e) {
+                throw new SQLException("MySQL JDBC driver not found.", e);
+            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+}
